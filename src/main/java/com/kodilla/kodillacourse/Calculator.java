26df790475740e8e03b487e349c4d7041fd015e1
MarcinSchmechel <marcin.schmechel@o2.pl>
2019-12-02 20:10:29 +0100

    Now it's ok

--- src/main/java/com/kodilla/kodillacourse/Calculator.java
+++ src/main/java/com/kodilla/kodillacourse/Calculator.java
@@ -1,5 +1,13 @@
 package com.kodilla.kodillacourse;
 
 public class Calculator {
+    int a;
+    int b;
 
+    public int addAtoB(int a, int b){
+        return a + b;
+    }
+    public int substractBfromA(int a, int b){
+        return a - b;
+    }
 }
--- src/main/java/com/kodilla/kodillacourse/KodillaCourseApplication.java
+++ src/main/java/com/kodilla/kodillacourse/KodillaCourseApplication.java
@@ -2,12 +2,19 @@ package com.kodilla.kodillacourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.kodilla.*;
+
 
 @SpringBootApplication
 public class KodillaCourseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaCourseApplication.class, args);
-    }
+        int a = 10;
+        int b = 5;
 
+        Calculator calculator = new Calculator();
+        System.out.println("Wynik dodawania " + a + " do " + b + " to " + calculator.addAtoB(a,b));
+        System.out.println("Wynik odejmowania " + b + " od " + a + " to " + calculator.substractBfromA(a,b));
+    }
 }
